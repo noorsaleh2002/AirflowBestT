42958706e8a658871358ffd14a7702d7cec47a6c
noorsaleh2002 <115577009+noorsaleh2002@users.noreply.github.com>
2025-10-07 17:17:08 +0300

    sensorfileAdded

---
+++ dags/sensor.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+
+from datetime import datetime
+from airflow.providers.standard.sensors.filesystem import FileSensor
+
+with DAG(
+dag_id="sensordag",
+start_date=datetime(2025,10,7),
+schedule="@daily",
+catchup=False,
+
+)as d:
+    waitingflie=FileSensor(
+        task_id="waitingfile",
+        poke_interval=30,
+        timeout=60*5 # 5 min
+        
+
+    )
